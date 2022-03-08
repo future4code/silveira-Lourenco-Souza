90c9501c6ff54fa3527ae3062899e2ce2ed5b4ad
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-03-07 22:27:10 -0300

    Exercício 12 resolvido

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -125,7 +125,11 @@ function retornaChamadaDeFilme(filme) {
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+    let anonimo = {
+        ...pessoa,
+        nome: "ANÔNIMO"
+    }
+    return anonimo
 }
 
 // EXERCÍCIO 13A
