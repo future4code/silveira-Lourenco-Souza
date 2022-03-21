caf84016c460ca3e86e19a6cebaca51d6c808ee2
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-03-21 16:30:21 -0300

    Exercício introdução ao React

---
+++ modulo2/jsx-labetube/jsx-labetube/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <label>Coloque aqui seu nome:
+        <input/>
+      </label>
+      <button>Enviar</button>
+      
+    </div>
+  );
+}
+
+export default App;
