69fafceb446143e35204f4f856bb8cd2ce90b2ff
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-03-23 17:25:14 -0300

    Função comentário implementada

--- modulo2/estados-insta4/insta4/src/components/SecaoComentario/SecaoComentario.js
+++ modulo2/estados-insta4/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -14,18 +14,21 @@ const InputComentario = styled.input`
 
 export class SecaoComentario extends Component {
 	state = {
-
+		comentario: ''
 	}
 
-	onChangeComentario = () => {
-
+	onChangeComentario = (event) => {
+		console.log(event.target.value);
+		this.setState({
+			comentario: event.target.value
+		})
 	}
 
 	render() {
 		return <CommentContainer>
 			<InputComentario
 				placeholder={'Comentário'}
-				value={''}
+				value={this.state.comentario}
 				onChange={this.onChangeComentario}
 			/>
 			<button onClick={this.props.aoEnviar}>Enviar</button>
