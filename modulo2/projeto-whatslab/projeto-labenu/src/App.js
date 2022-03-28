15f02fb63e46fffd553a1c8f9258797952d2dd55
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-03-27 22:20:19 -0300

    Implementado algumas estilizações

--- modulo2/projeto-whatslab/projeto-labenu/src/App.js
+++ modulo2/projeto-whatslab/projeto-labenu/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import background from './img/background.jpg'
+
 import Mensagem from './Componentes/Mensagem'
 
 const Container = styled.div`
-  max-width: 800px;
+  max-width: 700px;
   height: 100vh;
   border: solid black 1px;
   display: flex;
   flex-direction: column;
   flex-grow: 1;
   margin: 0 auto;
+  background-image: url('https://theabbie.github.io/blog/assets/official-whatsapp-background-image.jpg');
 `
 
 const CampoDasMensagens = styled.div`
--- modulo2/projeto-whatslab/projeto-labenu/src/Componentes/Mensagem.js
+++ modulo2/projeto-whatslab/projeto-labenu/src/Componentes/Mensagem.js
@@ -1,16 +1,33 @@
 import React from "react";
 import styled from 'styled-components'
 
+import send from '../img/send.png'
+
 const Container = styled.div`
     display: flex;
 `
 
 const CampoUsuario = styled.input`
-    width: 100px;
+    width: 150px;
+    height: 25px;
+    margin: 3px;
 `
 
 const CampoMensagem = styled.input`
     flex-grow: 1;
+    margin: 3px;
+`
+
+const SendImg = styled.img`
+    width: 20px;
+`
+
+const CustomButton = styled.button`
+    width: 45px;
+    margin: 3px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 
 export default class Mensagem extends React.Component {
@@ -20,7 +37,9 @@ export default class Mensagem extends React.Component {
             <Container>
                 <CampoUsuario type="text" />
                 <CampoMensagem type="text" />
-                <button>Enviar</button>
+                <CustomButton>
+                    <SendImg src={send} alt="" />
+                </CustomButton>
             </Container>
         )
     }
