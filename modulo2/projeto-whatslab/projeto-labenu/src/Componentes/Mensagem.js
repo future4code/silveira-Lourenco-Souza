ff903224af278a017899e4440d7950882ba0fbf0
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-03-27 21:32:50 -0300

    Adicionado componentes

---
+++ modulo2/projeto-whatslab/projeto-labenu/src/Componentes/Mensagem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Mensagem extends React.Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
