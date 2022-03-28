f9b3adcc86a892593b64525ba90eb91212555994
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-03-27 21:57:34 -0300

    Estilizações de containers principais prontas

--- modulo2/projeto-whatslab/projeto-labenu/src/App.js
+++ modulo2/projeto-whatslab/projeto-labenu/src/App.js
@@ -1,12 +1,40 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import Mensagem from './Componentes/Mensagem'
+
+const Container = styled.div`
+  max-width: 800px;
+  height: 100vh;
+  border: solid black 1px;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  margin: 0 auto;
+`
+
+const CampoDasMensagens = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 20px;
+  flex-grow: 1;
+`
+
 
 export default class App extends React.Component {
 
   render(){
     return(
-      <div>
-        <p>TESTE</p>
-      </div>
+      <Container>
+        <CampoDasMensagens>
+          <p>teste</p>
+          <p>teste</p>
+          <p>teste</p>
+        </CampoDasMensagens>
+
+        <Mensagem />
+      </Container>
     )
   }
 }
--- modulo2/projeto-whatslab/projeto-labenu/src/Componentes/Mensagem.js
+++ modulo2/projeto-whatslab/projeto-labenu/src/Componentes/Mensagem.js
@@ -1,12 +1,27 @@
 import React from "react";
+import styled from 'styled-components'
+
+const Container = styled.div`
+    display: flex;
+`
+
+const CampoUsuario = styled.input`
+    width: 100px;
+`
+
+const CampoMensagem = styled.input`
+    flex-grow: 1;
+`
 
 export default class Mensagem extends React.Component {
 
     render() {
         return (
-            <div>
-                
-            </div>
+            <Container>
+                <CampoUsuario type="text" />
+                <CampoMensagem type="text" />
+                <button>Enviar</button>
+            </Container>
         )
     }
 }
