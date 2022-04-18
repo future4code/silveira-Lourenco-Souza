f43b728ecfa3cde87cdc7c9c07ee57ce823877d2
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-04-18 17:39:09 -0300

    Exercício concluído

--- modulo3/use-state/insta-lab-hooks/src/components/SecaoComentario/SecaoComentario.js
+++ modulo3/use-state/insta-lab-hooks/src/components/SecaoComentario/SecaoComentario.js
@@ -14,8 +14,11 @@ const InputComment = styled.input `
 
 const SecaoComentario = (props) => {
 
+  const [comentario, setComentario] = useState('')
+
 
 	const onChangeComentario = (event) => {
+    setComentario(event.target.value)
 	}
 
 	return (
@@ -23,10 +26,10 @@ const SecaoComentario = (props) => {
 			<InputComment
 				className={'input-comentario'}
 				placeholder={'Comentário'}
-				value={""}
+				value={comentario}
 				onChange={onChangeComentario}
 			/>
-			<button onClick={() => { props.enviarComentario() }} >Enviar</button>
+			<button onClick={() => { props.enviarComentario(comentario) }} >Enviar</button>
 		</CommentContainer>
 	)
 }
