c0d936c30833f1f2cc62f32b74e7758c3144340b
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-04-19 22:31:24 -0300

    Exercício concluído

--- modulo3/use-effect/poke-api/src/components/PokeCard/PokeCard.js
+++ modulo3/use-effect/poke-api/src/components/PokeCard/PokeCard.js
@@ -1,47 +1,29 @@
 import React from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
-class PokeCard extends React.Component {
-  state = {
-    // valor do estado que guarda infos e foto do pokemon
-    pokemon: {}
-  };
+function PokeCard (props) {
 
-  // método que roda após a montagem do componente
-  componentDidMount() {
-    this.pegaPokemon(this.props.pokemon);
-  }
 
-  // método que roda após a atualização do componente.
-  // Um dos casos de atualização do componente é a
-  // mudança da props que está sendo passado pra ele
-  componentDidUpdate(prevProps) {
-    // aqui, é feita uma verificação da props anterior com a props atual.
-    // Caso a props anterior seja diferente da props atual,
-    // a função pegaPokemon é chamada.
-    if (prevProps.pokemon !== this.props.pokemon) {
-      this.pegaPokemon(this.props.pokemon);
-    }
-  }
+  const [pokemon, setPokemon] = useState([])
 
-  // função que bate na poke API com um nome específico de pokemon
-  // Isso permite que consigamos pegar as infos dos pokemons.
-  // Nos métodos de ciclo de vida, ela é chamada passando como
-  // parâmetro o nome de pokemon que está chegando como props.
-  pegaPokemon = pokeName => {
+  const pegaPokemon = pokeName => {
     axios
       .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
       .then(response => {
-        // guarda as infos do pokemon no estado
-        this.setState({ pokemon: response.data });
+        setPokemon(response.data)
       })
       .catch(err => {
         console.log(err);
       });
   };
 
-  render() {
-    const pokemon = this.state.pokemon;
+
+  useEffect (() => {
+    pegaPokemon(props.pokemon)
+  }, [pokemon])
+
+  // const details = pokemon;
 
     return (
       <div>
@@ -53,7 +35,6 @@ class PokeCard extends React.Component {
         )}
       </div>
     );
-  }
 }
 
 export default PokeCard;
