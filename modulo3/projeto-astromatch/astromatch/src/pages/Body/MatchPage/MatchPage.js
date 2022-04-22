f307d73cc60802cd08cc7a7859d25fbe3409a2f9
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-04-22 00:44:04 -0300

    Integração com a Api 1/3

--- modulo3/projeto-astromatch/astromatch/src/pages/Body/MatchPage/MatchPage.js
+++ modulo3/projeto-astromatch/astromatch/src/pages/Body/MatchPage/MatchPage.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
 
+import {BASE_URL} from '../../../constants/URL'
 import {Container} from './MatchPage-style'
 
+const ContainerPerson = styled.div`
+  border: dotted 1px black;
+  display: flex;
+  align-items: center;
+  margin: 10px;
+`
+const Img = styled.img`
+  height: 50px;
+  width: 50px;
+  border-radius: 90px;
+`
+const Name = styled.p`
+  font-size: 20px;
+  margin: 10px;
+`
+
 const MatchPage = () => {
+
+  const [listaMatch, setListaMatch] = useState([])
+
+  useEffect(() => {
+    getMatch()
+  }, [])
+
+  const getMatch = () => {
+    axios
+    .get(`${BASE_URL}/matches`)
+    .then((res) => {
+      // console.log(res.data.profile);
+      setListaMatch(res.data.matches)
+    })
+    .catch((err) => {
+      console.log(err.response.data);
+    })
+  }
+
+  const mapMatches = listaMatch.map((person) => {
+    return (
+      <ContainerPerson>
+        <Img src={person.photo} alt={`foto de ${person.name}`} />
+        <Name>{person.name}</Name>
+      </ContainerPerson>
+    )
+  })
+
+  console.log(listaMatch);
   return (
     <Container>
-      Uma lista de matches aqui:
+      {mapMatches}
     </Container>
   )
 }
