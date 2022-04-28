1d5e2f86d61440e18b800545e7b1ba3597adddc3
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-04-28 20:19:51 -0300

    Implementada página de lista de viagens

--- modulo3/projeto-labex/labex/src/pages/ListTripsPage/ListTrips.js
+++ modulo3/projeto-labex/labex/src/pages/ListTripsPage/ListTrips.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
+import {BASE_URL} from '../../constants/URL'
+import TripCard from '../../components/TripCard/TripCard'
 
 const ListTrips = () => {
+
+  const [trips, setTrips] = useState([])
+
+
+  // ----------------------------------------------------------------------------
+  // Funções de navegação
 
   const navigate = useNavigate()
 
@@ -18,12 +27,47 @@ const ListTrips = () => {
     navigate("/trips/application")
   }
 
+    // ----------------------------------------------------------------------------
+
+    const getTrips = () => {
+
+        
+
+      axios
+        .get(`${BASE_URL}/trips`)
+      .then((res) => {
+        setTrips(res.data.trips)
+      })
+      .catch((err) => {
+        console.log(err.data.response);
+      })
+    }
+
+      useEffect(() => {
+        getTrips()
+      }, [])
+
+    // ----------------------------------------------------------------------------
+
+    const listTrips = trips.map(element => {
+        return (
+          < TripCard key={element.id} tripsContent={element} />
+        )
+    })
+
+    // ----------------------------------------------------------------------------
+
 
   return (
     <div> <h1>ListTrips</h1 >
         <button onClick={goToHomePage} >HomePage</button>
         <button onClick={goToLoginPage} >LoginPage</button>
         <button onClick={goToApplicationFormPage} >ApplicationFormPage</button>
+
+        {trips.length === 0 && <p>Carregando...</p>}
+        {listTrips}
+        
+
     </div>
   )
 }
