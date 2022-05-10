cfa2aa493f851d91e9f8839a1bafb43a72d3dc1b
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-05-10 09:54:50 -0300

    Estrutura básica criada

---
+++ modulo3/projeto-LabEddit/labeddit/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { GlobalStyled } from './global/GlobalStyled'
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyled />
+    </>
+  )
+}
+
+export default App
---
+++ modulo3/projeto-LabEddit/labeddit/src/global/GlobalStyled.js
@@ -0,0 +1,10 @@
+import styled, {createGlobalStyle} from "styled-components"
+
+export const GlobalStyled = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    user-select: none;
+  }
+`
