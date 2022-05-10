a2d1eeb66c853821bd4877500682ee4a993e5399
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-05-10 20:03:56 -0300

    Páginas criadas 2/4 (SignupPage) v2

--- modulo3/projeto-LabEddit/labeddit/src/pages/SignupPage/SignupPage.js
+++ modulo3/projeto-LabEddit/labeddit/src/pages/SignupPage/SignupPage.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const ContainerMain = styled.div`
+  margin: auto;
+  min-height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+    div{
+      display: flex;
+      flex-direction: column;
+    }
+`
 
 const SignupPage = () => {
   return (
-    <>
+    <ContainerMain>
       <p>Header</p>
       <h1>Olá, boas vindas ao LabEddit ;)</h1>
 
@@ -13,14 +28,14 @@ const SignupPage = () => {
       </div>
 
       <div>
-        <h2>Ao continuar, você concorda com o nosso <span>Contrato de usuário</span> e nossa <span>Política de Privacidade</span></h2>
+        <p>Ao continuar, você concorda com o nosso <span>Contrato de usuário</span> e nossa <span>Política de Privacidade</span></p>
         <div>
           <input type="checkbox" id='sendEmail' />
           <label htmlFor="sendEmail">Eu concordo em receber emails sobre coisas legais no LabEddit</label>
         </div>
         <button>Cadastrar</button>
       </div>
-    </>
+    </ContainerMain>
   )
 }
 
