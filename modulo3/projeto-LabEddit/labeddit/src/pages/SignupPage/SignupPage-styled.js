4ee54b037f392ad50346bbd8c9b02a8bda1e301b
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-05-10 20:05:09 -0300

    Páginas criadas 2/4 (SignupPage) v3

---
+++ modulo3/projeto-LabEddit/labeddit/src/pages/SignupPage/SignupPage-styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const ContainerMain = styled.div`
+  margin: auto;
+  min-height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+    div{
+      display: flex;
+      flex-direction: column;
+    }
+`
--- modulo3/projeto-LabEddit/labeddit/src/pages/SignupPage/SignupPage.js
+++ modulo3/projeto-LabEddit/labeddit/src/pages/SignupPage/SignupPage.js
@@ -1,19 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
 
-const ContainerMain = styled.div`
-  margin: auto;
-  min-height: 100vh;
-  width: 100vw;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-    div{
-      display: flex;
-      flex-direction: column;
-    }
-`
+import { ContainerMain } from './SignupPage-styled'
 
 const SignupPage = () => {
   return (
