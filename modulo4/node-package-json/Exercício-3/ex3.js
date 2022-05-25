5c2a96486b412d19a9b7f1396b217f832bc705e8
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-05-25 01:13:07 -0300

    Desefio 3

--- modulo4/node-package-json/Exercício-3/ex3.js
+++ modulo4/node-package-json/Exercício-3/ex3.js
@@ -1,8 +1,22 @@
 // Exercício 3
 
-const tarefa = process.argv[2]
-const listaTarefa = {tarefas: ["Nadar", "Fazer nada"]}
+const fs = require('fs')
+const listaTarefa = JSON.parse(fs.readFileSync("./listaTarefas.json", 'utf8'))
 
-listaTarefa.tarefas.push(tarefa)
-console.log("Tarefa adicionada com sucesso!");
-console.log(listaTarefa);
+const tarefa = process.argv[2]
+
+if(tarefa === undefined){
+  console.log("nenhum argumento foi inserido");
+} else {
+
+  listaTarefa.tarefas.push(tarefa)
+
+  fs.writeFile("./listaTarefas.json", JSON.stringify(listaTarefa), err => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("Tarefa adicionada com sucesso!");
+  })
+
+  console.log(listaTarefa);
+}
