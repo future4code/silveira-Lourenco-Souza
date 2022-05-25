b09467dec72f933a4ce8859d620d77f8b74807f5
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-05-24 22:59:13 -0300

    Exercício 2.

--- modulo4/node-package-json/index.js
+++ modulo4/node-package-json/index.js
@@ -3,7 +3,30 @@
 // Resposta: usando o process.argv
 
 // b)
-const nome = process.argv[2]
-const idade = +process.argv[3]
+// const nome = process.argv[2]
+// const idade = +process.argv[3]
 
-console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${idade + 7}`);
+// console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${idade + 7}`);
+
+// ---------------------------------------------------------------------------------------------------
+
+// Exercício 2
+
+// const operacao = process.argv[2]
+// const arg1 = +process.argv[3]
+// const arg2 = +process.argv[4]
+
+// switch(operacao){
+//   case "add":
+//     console.log(`Resposta: ${arg1 + arg2}`);
+//     break
+//   case "sub":
+//     console.log(`Resposta: ${arg1 - arg2}`);
+//     break
+//   case "mult":
+//     console.log(`Resposta: ${arg1 * arg2}`);
+//     break
+//   case "div":
+//     console.log(`Resposta: ${arg1 / arg2}`);
+//     break
+// }
