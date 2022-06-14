e56d9df9491ee589705adca106035671c904757d
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-06-13 22:49:57 -0300

    Exercicio 02

--- modulo4/async-await-promises/src/exercicio01.ts
+++ modulo4/async-await-promises/src/exercicio01.ts
@@ -1,13 +1,10 @@
 import axios from "axios";
 import { BASE_URL } from "./constants/url";
 
-// ###########################################################################################
-// Exercício 1
-
 // a) https://labenews.herokuapp.com/subscribers
 // b) ?
 
-const getSuscribes = async (): Promise<any[]>=> {
+async function getSuscribes(): Promise<any[]>{
   
   const result = await axios.get(`${BASE_URL}/subscribers`)
   return result.data
