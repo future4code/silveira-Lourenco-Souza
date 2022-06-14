44bd74acd214b42002c34dca16d5ca2a1f931001
Dev-Dsouza <lourenco.h.colares@gmail.com>
2022-06-13 23:38:43 -0300

    Exercicio 5 parte 01

--- modulo4/async-await-promises/src/exercicio05.ts
+++ modulo4/async-await-promises/src/exercicio05.ts
@@ -1,5 +1,29 @@
 import axios from "axios";
 import { BASE_URL } from "./constants/url";
+import { news, user } from "./constants/types";
+
+const myNews: news = {
+  title: "Mais frio vem por aí!",
+  content: "Uma onda de frio está à caminho para deixar o clima ainda mais congelado.",
+  date: Date.now()
+}
 
 
 
+const main = async (news: news): Promise<void> => {
+  
+  try{
+
+    createNews(news)
+    const subscribers = await getAllSubscribers()
+    const subscribersIds = getSubscribersIds(subscribers)
+    await notifyAllsubscribers(subscribersIds)
+
+  } catch (error: any){
+    const resp = error.response?.data || error.message
+    console.log(resp);
+    
+  }
+}
+
+main(myNews)
